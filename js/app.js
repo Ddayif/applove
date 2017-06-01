e456c259aea15d4e21c28ce059d034a62f043293
Joselin Grez <joselingrez.m@hotmail.es>
2017-05-31 22:55:49 -0300

    Empezando a agregar funcionalidad

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@
  * Archivo principal de funcionalidad de JS
  */
 
+ (function imagenes(){
+ 	var boxes = Array.from(document.getElementsByClassName("col-4"));
+ 	var modal = document.getElementById("container");
+	var bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			alert("Hola Mundo");
+		})
+	})
+})()
